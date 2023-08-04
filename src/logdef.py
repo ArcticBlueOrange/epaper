c48e019a69ba7cba762e3513cdb11f8d24bb08e5
ArcticBlueOrange <brando1411@gmail.com>
2023-08-04 13:59:59 +0200

    fixed logging script

--- src/logdef.py
+++ src/logdef.py
@@ -5,35 +5,34 @@ from datetime import datetime as dt
 
 def get_logger(
         name,
-        filedir='logs',
-        ts_format='%Y-%m-%d_%H_%M_%S',
         level='info',
-        format='%(asctime)s | %(levelname)s | %(message)s',
+        filedir="logs",
+        filename='epaper',
+        ts_format='%Y-%m-%d_%H_%M_%S',
+        text_format='%(asctime)s | %(filename)s | %(funcName)s | %(levelname)s | %(message)s',
         to_console=True,
+        console_format=None,
 ):
     lvl = {
         'critical': logging.CRITICAL, 'error': logging.ERROR,
         'warning': logging.WARNING, 'info': logging.INFO,
         'debug': logging.DEBUG, 'notset': logging.NOTSET,
     }.get(level, logging.INFO)
-    logger = logging.Logger(
+
+    ts = dt.today().strftime(ts_format)
+
+    logging.basicConfig(
         level=lvl,
-        name=name,
+        format=text_format, 
+        filename=f"{filedir}/{name}_{ts}.log",
+        filemode='a',
     )
 
-    formatter = logging.Formatter(format)
-
-    if filedir:
-        ts = dt.now().strftime(ts_format)
-        # print(f"{filedir}/{name}_{ts}.log")
-        handler = logging.FileHandler(f"{filedir}/{name}_{ts}.log")
-        handler.setFormatter(formatter)
-        logger.addHandler(handler)
 
     if to_console:
-        # writing to stdout
-        handler = logging.StreamHandler(sys.stdout)
-        handler.setFormatter(formatter)
-        logger.addHandler(handler)
+        formatter = logging.Formatter(console_format if console_format else text_format)
+        console = logging.StreamHandler(sys.stdout)
+        console.setFormatter(formatter)
+        logging.getLogger('').addHandler(console)
 
-    return logger
+    return logging.getLogger(name)
